0cefcdb1d70499cff336d483c1e9e2453a7bcb06
Your Name <rahul.kathiriya.61@gmail.com>
2022-08-15 20:41:34 +0530

    calculater

---
+++ JAVA_SCRIPT/calculater.js
@@ -0,0 +1,25 @@
+
+let result = document.getElementById("result");
+
+
+function display(val) {
+    result.value += val;
+    return val;
+}
+
+
+function solve() {
+
+    let x = result.value;
+    let y = eval(x);
+
+    result.value = y;
+    return y;
+}
+
+
+function clearScreen() {
+
+    result.value = ' ';
+
+}
